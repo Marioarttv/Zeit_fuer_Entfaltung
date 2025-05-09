bfa0f3b2db4625f62964d04f7471305c4ffc2849
Marioarttv <Marioarttv@gmail.com>
2025-05-10 01:32:21 +0200

    Newest version

--- Zeit_für_Entfaltung/Program.cs
+++ Zeit_für_Entfaltung/Program.cs
@@ -1,3 +1,6 @@
+using Blazorise;
+using Blazorise.Bootstrap5;
+using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Zeit_für_Entfaltung;
@@ -5,6 +8,15 @@ using Zeit_für_Entfaltung;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+
+builder.Services
+    .AddBlazorise(options =>
+    {
+        options.Immediate = true;
+    })
+    .AddBootstrap5Providers()
+    .AddFontAwesomeIcons();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
